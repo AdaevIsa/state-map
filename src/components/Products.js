e1d9d43ddd4eed5c6ba2a22e060ba47d1caa5134
isa <rasul.228.rasul@mail.ru>
2024-03-15 22:12:49 +0300

    larek

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./table-styles.css";
+import { fakeDatabase } from "../fakeDatabase";
+
 
 function Products() {
+  const [count, setCount] = useState(fakeDatabase)
+  const  Removes = (id) => {
+    const arr = count.filter(item => item.id !== id)
+    setCount(arr)
+  }
+
   return (
     <div className="my-2">
       <table className="table">
@@ -14,7 +22,21 @@ function Products() {
             <th>Действие</th>
           </tr>
         </thead>
-        <tbody></tbody>
+       {count.map(isa => {
+        return (
+          <tbody>
+          <tr>
+            <td><img src={isa.image}></img></td>
+            <td>{isa.name}</td>
+            <td>{isa.price}</td>
+            <td>{isa.inStock ? "есть" : "нет"}</td>
+            <td><button onClick={() => Removes(isa.id)}>удалить</button></td>
+          </tr>
+          
+        </tbody>
+        )
+       })}
+       
       </table>
     </div>
   );
